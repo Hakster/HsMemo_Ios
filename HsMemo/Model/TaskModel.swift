ff1dbe663d17246321e9c95cb22f2e43e1e51bfb
Maxim Kozlov <hakster@yandex.ru>
2018-11-05 22:26:55 +0300

    Add files via upload
    Add local notification

--- HsMemo/Model/TaskModel.swift
+++ HsMemo/Model/TaskModel.swift
@@ -5,8 +5,11 @@
 //  Created by Максим Козлов on 20/10/2018.
 //  Copyright © 2018 Максим Козлов. All rights reserved.
 //
-
 import Foundation
+import UserNotifications
+
+// Структура данных
+
 
 // Массив списка Tasks
 // Set если передаются новые данные то сохраняем
@@ -32,9 +35,50 @@ func addTask(title: String, isComplited: Bool = false, deadDate: Date){
 // Метод меняет статус выполнен/не выполнен
 func changeState(at item: Int) -> Bool{
     taskData[item]["isComplited"] = !(taskData[item]["isComplited"] as! Bool)
+    // если выполнено то удалить уведомление иначе на оборот
+    if  (taskData[item]["isComplited"] as! Bool == true){
+        removeNotifications(withIdentifiers: [taskData[item]["Title"] as! String])
+    }else{
+        scheduleNotification(indetifiers: taskData[item]["Title"] as! String,
+                             title: taskData[item]["Title"] as! String,
+                             deadDate: taskData[item]["DeadDate"] as! Date) { (success) in
+            if (success){
+                print("GOOOD")
+            }
+        }
+    }
+    
     return taskData[item]["isComplited"] as! Bool
 }
 // Метод удаления Task из списка
 func removeItem(at index: Int){
+    removeNotifications(withIdentifiers: [taskData[index]["Title"] as! String])
     taskData.remove(at: index)
+}
+
+// Функция создани уведомления
+func scheduleNotification(indetifiers: String, title: String , deadDate date: Date, completion: (Bool) -> ()) {
+    
+    // Создание контента уведомления
+    let content = UNMutableNotificationContent()
+    content.title = "Напоминание!"
+    content.body = title
+    content.sound = UNNotificationSound.default
+    
+    // Устанавливаем вид календаря
+    let calendar = Calendar(identifier: .gregorian)
+    let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+    let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+    let request = UNNotificationRequest(identifier: indetifiers, content: content, trigger: trigger)
+    
+    // Запрашиваем центер уведомлений
+    let center = UNUserNotificationCenter.current()
+    // И добавляем наже уведомление в центр
+    center.add(request, withCompletionHandler: nil)
+}
+
+// Удаление уведомления
+func removeNotifications(withIdentifiers indetifiers: [String]) {
+    let center = UNUserNotificationCenter.current()
+    center.removePendingNotificationRequests(withIdentifiers: indetifiers)
 }
